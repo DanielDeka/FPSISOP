fa22878439b4b0f1cccd723f79e7d8d0b48e00d7
Daniel Kurniawan <dekaslr8@gmail.com>
2017-12-06 18:34:13 +0700

    Tambah

--- sleep.c
+++ sleep.c
@@ -9,4 +9,10 @@ int main (int argc, char *argv[])
 		printf("2, Kegunaan: durasi sleep\n");
 		exit();
 	}
+
+	durasi = atoi(argv[1]);
+	if(durasi > 0) sleep(durasi);
+	else printf(2,"Interval tidak valid %s\n", argv[1]);
+
+	exit();
 }
