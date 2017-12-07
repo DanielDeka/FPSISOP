bb0957a4a56890d08f9999ad794426522c0520da
nabilhdrhmn <33083690+nabilhdrhmn@users.noreply.github.com>
2017-12-07 17:59:01 +0700

    masih nyoba2

--- mv.c
+++ mv.c
@@ -1,1 +1,14 @@
 
+#include "types.h"
+#include "stat.h"
+
+int main(int argc, char argv[])
+{
+  if(argc != 3){
+     printf(1, "Please, input the command as [mv src_file dest_file]\n");
+     exit();
+  }
+  if((link(argv[1], argv[2]) < 0) || (unlink(argv[1]) < 0)){
+  }
+  exit();
+}
