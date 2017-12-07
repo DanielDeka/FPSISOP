d7ac7955b4612fbbbe4b62d4282a5c02804e3a84
reynmraw <33017954+reynmraw@users.noreply.github.com>
2017-12-07 21:06:55 +0700

    Update supaya minimal 2 kata klo mau touch

--- touch.c
+++ touch.c
@@ -1,1 +1,17 @@
+#include "xv6/types.h"
+#include <syscall.h>
+#include "xv6/stat.h"
+#include "xv6/fcntl.h"
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
 
+int
+main(int argc, char *argv[])
+{
+ int i;
+ int file;
+ if (argc < 2){
+  printf("Usage: touch files...\n");
+  sysexit();
+ }
