68e66cbb3abe22aece7922ef762a91dd90701d12
reynmraw <33017954+reynmraw@users.noreply.github.com>
2017-12-08 01:58:15 +0700

    fungsi pathnya dan situasi error

--- ls.c
+++ ls.c
@@ -1,1 +1,22 @@
+#include <syscall.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <xv6/stdio.h>
+#include <xv6/dirent.h>
+#include <xv6/stat.h>
+#include <xv6/fcntl.h>
+
+int pathstat(char *n, struct stat *st)
+{
+  int fd;
+  int r;
+
+  fd = open(n, O_RDONLY);
+  if(fd < 0)
+    return -1;
+  r = fstat(fd, st);
+  close(fd);
+  return r;
+}
 
