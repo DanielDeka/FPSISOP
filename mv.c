a04f5040102aa8abee0944c82b8a07c62c360a94
nabilhdrhmn <33083690+nabilhdrhmn@users.noreply.github.com>
2017-12-08 23:31:57 +0700

    insyaAllah fix mv sm mv *

--- mv.c
+++ mv.c
@@ -4,6 +4,70 @@
 #include "fcntl.h"
 #include "fs.h"
 
+void cpb(char *path, char *path1){
+    char a[512], *p;
+    char *temp_1 = malloc(100);
+    memset(temp_1, 0, sizeof temp_1);
+    char *temp_2 = malloc(100);
+    memset(temp_2, 0, sizeof temp_2);
+    int fd;
+    struct dirent d;
+    struct stat st;
+
+    if((fd = open(path, 0)) < 0){
+        printf(2, "cp: cannot open %s\n", path);
+        return;
+    }
+
+    if(fstat(fd, &st) < 0){
+        printf(2, "cp: cannot stat %s\n", path);
+        return;
+    }
+
+    switch(st.type){
+        case T_FILE:
+            return;
+
+        case T_DIR:
+            if(strlen(path) + 1 + DIRSIZ + 1 > sizeof a){
+                printf(1, "ls: path too long\n");
+                break;
+            }
+
+        strcpy(a, path);
+        p = a+strlen(a);
+        *p++ = '/';
+        mkdir_(path1);
+        while(read(fd, &d, sizeof(d) == sizeof(d))){
+            if(d.inum == 0)
+                continue;
+            memmove(p, d.name, DIRSIZ);
+            p[DIRSIZ] = 0;
+            if(stat(a, &st) < 0){
+                printf(1, "ls: cannot stat %s\n", a);
+                continue;
+            }
+            strcpy(temp_1, path);
+            strcat(temp_1, "/");
+            strcat(temp_1, fmtname(a));
+            strcat(temp_1, "\0");
+            strcpy(temp_2, path1);
+            strcat(temp_2, "/");
+            strcat(temp_2, fmtname(a))
+            strcat(temp_2, "\0");
+            if(strcmp(fmtname(a), ".a") == 0)
+                continue;
+            if(strcmp(fmtname(a), ".") == 0)
+                continue;
+            if(strcmp(fmtname(a), "..") == 0)
+                continue;
+            move(temp_1, temp_2);
+        }
+        break;
+    }
+    close(fd);
+}
+
 char* strcat(char* s1, char* s2){
     char* b = s1;
     while(*s1)
@@ -103,7 +167,25 @@ int main(int argc, char argv[])
     strcpy(argv[3], temp);
     printf(1, "%s\n", temp);
   }
-  
+  if(strcmp(argv[3], "*") == 0){
+    int k = open(".a", 0_CREATE | 0_RDWR);
+    char *a = malloc(100);
+    memset (a, 0, sizeof a);
+    read(k, a, sizeof(a));
+    char *temp_1 = malloc(100);
+    memset(temp_1, 0, sizeof temp_1);
+    if(*argv[2]!='/')
+        strcat(temp_1, a);
+    strcat(temp_1, argv[2]);
+    char *temp_2 = malloc(100);
+    memset(temp_2, 0, sizeof temp_1);
+    if(*argv[3]!='/')
+        strcat(temp_1, a);
+    strcat(temp_2, argv[3]);
+    cpb(temp_1, temp_2);
+  }
+  else
+    move(argv[2], argv[3]);
   exit();
 }
 
