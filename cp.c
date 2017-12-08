dc910c4cb151728c735274c3c64de267545e2df5
stradzzz <33017938+stradzzz@users.noreply.github.com>
2017-12-08 22:53:20 +0700

    Update cp.c

--- cp.c
+++ cp.c
@@ -9,23 +9,32 @@ int
 main(int argc, char *argv[])
 {
   int fd, dfd, r;
+  char *src;
+  char *dest;
+  
   if(argc <= 2){
     printf(1, "Pakai 2 argumen\n");
     exit();
   }
   
-  if ((fd = open(argv[1], O_RDONLY)) < 0) {
+  src = argv[1];
+  dest = argv[2];
+  
+  if ((fd = open(src, O_RDONLY)) < 0) {
     printf(1, "cp: cannot open source %s\n", argv[1]);
     exit();
   }
-  if ((dfd = open(argv[2], O_CREATE|O_RDWR)) < 0) {
+  if ((dfd = open(dest, O_CREATE|O_RDWR)) < 0) {
     printf(1, "cp: cannot open destination %s\n", argv[2]);
     exit();
   }
   
   while ((r = read(fd, buf, sizeof(buf))) > 0) {
     w = write(dfd, buf, r);
-    
+    if (w != r || w < 0) 
+      break;
+  }
+  
   close(fd);
   close(dfd);
 
