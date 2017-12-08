4e48fe5e27317a3cafdd5d77a696181bceea3540
nabilhdrhmn <33083690+nabilhdrhmn@users.noreply.github.com>
2017-12-08 10:35:13 +0700

    hasil kemarin nyari referensi

--- rm.c
+++ rm.c
@@ -1,1 +1,20 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
 
+int main(int argc, char *argv[]){
+  int n;
+  if(argc < 2){
+    printf(2, "Usage: rm files...\n");
+    exit();
+  }
+  
+  for(n = 1; n < argc; n++){
+    if(unlink(argv[i]) < 0){
+      printf(2, "rm: %s failed to delete\n", argv[i]);
+      break;
+    }
+  }
+
+  exit();
+}
