50c49b3431e28bac4c3e037f2062d2583aa1ee2f
Daniel Kurniawan <dekaslr8@gmail.com>
2017-12-09 12:12:54 +0700

    Ubah jadi porting ke nonport

--- rename.c
+++ rename.c
@@ -1,8 +1,32 @@
-#include <stdio.h>
-#include <assert.h>
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
 
-int rename(const char* src, const char* dst)
-{
-  assert(0 && "not implemented yet.");
-  return 0;
+char buff[600];
+
+int main(int argc, char *argv[]){
+
+	int f1,f2,n;
+	if (argc<2)
+	{
+		printf(2, "Kegunaan: ganti nama file\n");
+		exit();
+	}
+
+	if((f1 = open(argv[1], O_RDONLY)) < 0) {
+		printf(1, "gagal buka file %s\n",argv[1]);
+		exit();
+	}
+
+	f2 = open(argv[2], O_CREATE|O_RDWR)
+
+	while((n=read(file1, buff,sizeof(buff))) > 0){
+		write(file2,buff,n);
+		}
+
+	close(f1);
+	close(f2);
+	
+	exit();
 }
